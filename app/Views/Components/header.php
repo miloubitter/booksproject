a5044d7c712786591cc427758560f61f74747455
Milou Bitter <milou.bitter@youngcoders.nl>
2018-08-31 11:44:03 +0200

    -margin-bottom weggehaald bij de navbar

--- app/Views/Components/header.php
+++ app/Views/Components/header.php
@@ -17,7 +17,7 @@
     <script src="js/init.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm mb-2">
+<nav class="navbar navbar-expand-sm">
     <a class="headerlogo navbar-brand " href="?route=index">Milou's Bookstore</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
