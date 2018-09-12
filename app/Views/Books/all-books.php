f31dd5df8f2d2fc9d63326381fea929c6833de26
Milou Bitter <milou.bitter@youngcoders.nl>
2018-09-12 18:03:17 +0200

    - scrollable tabel

--- app/Views/Books/all-books.php
+++ app/Views/Books/all-books.php
@@ -4,41 +4,39 @@
         <p class="lead textP text-center">Welcome to the book catalog. <br /> Click on the books to see their details!.</p>
     </div>
 </div>
-<!--<br />-->
-<!--<h1 class="mt-5">--><?php //echo $viewModel['pageTitle']?><!--</h1>-->
 
 <div class="container">
     <div class="row">
         <div class="col">
-<!--            <p class="intro"> <br /> Welcome to the book catalog. <br /> Click on the books to see their details!</p>-->
+            <div class="scrollable">
+                <table class="table">
+                    <thead class="thead-light">
+                    <tr>
+                        <th scope="col">Book cover</th>
+                        <th scope="col">Title</th>
+                        <th scope="col" class="d-none d-sm-table-cell">Author</th>
+                        <th scope="col" class="d-none d-sm-table-cell">Category</th>
+                        <th scope="col" class="d-none d-sm-table-cell">ISBN</th>
+                        <th scope="col">Price</th>
+                    </tr>
+                    </thead>
 
-            <table class="table">
-                <thead class="thead-light">
-                <tr>
-                    <th scope="col">Book cover</th>
-                    <th scope="col">Title</th>
-                    <th scope="col" class="d-none d-sm-table-cell">Author</th>
-                    <th scope="col" class="d-none d-sm-table-cell">Category</th>
-                    <th scope="col" class="d-none d-sm-table-cell">ISBN</th>
-                    <th scope="col">Price</th>
-                </tr>
-                </thead>
+                <?php
+                //    foreach($viewModel['books'] as $id=>$book){
+                //        echo "
+                //        <tr onclick=\"window.location = '?route=show&id={$book['id']}'\">
+                //            <td>{$book['title']}</a></td>
+                //            <td class=\"d-none d-sm-table-cell\">{$book['author_id']}</td>
+                //            <td class=\"d-none d-sm-table-cell\">{$book['isbn']}</td>
+                //            <td>{$book['price']}</td>
+                //        </tr>
+                //        ";
+                //    }
+                //
+                //    ?>
 
-            <?php
-            //    foreach($viewModel['books'] as $id=>$book){
-            //        echo "
-            //        <tr onclick=\"window.location = '?route=show&id={$book['id']}'\">
-            //            <td>{$book['title']}</a></td>
-            //            <td class=\"d-none d-sm-table-cell\">{$book['author_id']}</td>
-            //            <td class=\"d-none d-sm-table-cell\">{$book['isbn']}</td>
-            //            <td>{$book['price']}</td>
-            //        </tr>
-            //        ";
-            //    }
-            //
-            //    ?>
-
-            </table>
+                </table>
+            </div>
 
             <br clear="all" />
 
