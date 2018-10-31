f98ea276d2a3c6a6a55863aff5162bcfb2b5dd71
Milou Bitter <milou.bitter@youngcoders.nl>
2018-10-31 11:55:30 +0100

    -UserRepositoryInterface

---
+++ app/Models/Interfaces/UserRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Interfaces;
+
+use App\Models\Entities\User;
+
+interface UserRepositoryInterface
+{
+    public function getUser(int $id) : ?User;
+
+    public function getAllUsers() : array;
+
+    public function login(string $email, string $password) : ?User;
+}
